2beaaf39e3c9e6cde57325adcef5832c11877df6
root <root@ip-172-31-36-113.us-west-2.compute.internal>
2017-08-30 18:56:01 +0900

    add css files

--- application/views/form/header.php
+++ application/views/form/header.php
@@ -1,6 +1,12 @@
-<html>
+<!DOCTYPE html>
+<html lang="ja">
 	<head>
+		<meta charset="UTF-8">
 		<title>Zaim Data Analysis</title>
+		<link href="css/reset.css" rel="stylesheet">
+		<link href="css/css.css" rel="stylesheet">
 	</head>
 	<body>
-	<h2><?php echo $title; ?></h2>
+	<header>
+	<h1><?php echo $title; ?></h1>
+	</header>
